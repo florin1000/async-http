dbba54456bf0d00b24c25da86421ce9cf78d99cb
Florin <oancea.quentin.florin@gmail.com>
2020-09-14 13:44:40 +0300

    remove default data from initialize angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShowDataFromApiComponent } from './components/show-data-from-api/show-data-from-api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowDataFromApiComponent
   ],
   imports: [
     BrowserModule
