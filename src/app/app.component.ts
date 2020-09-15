1e8b6efc6a4981c9ff4f1c7c4d0fb99042a32c9a
Florin <oancea.quentin.florin@gmail.com>
2020-09-15 11:12:07 +0300

    filter content functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UsersService } from './services/users.service'
 import { Observable } from 'rxjs';
+import { defaultIfEmpty, filter, map } from 'rxjs/operators';
 
 
 
@@ -9,15 +10,28 @@ import { Observable } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  users$: Observable<any>
+  users$: Observable<any>;
+  bufferUsers$: Observable<any>;
+  filter = false;
   constructor(private userService: UsersService) {
 
   }
+
   ngOnInit(): void {
     this.getUsers()
   }
+
   getUsers() {
     this.users$ = this.userService.getUsers();
+    this.bufferUsers$ = this.users$;
+  }
+
+  filterUsers() {
+    //filter users, criteria starting letter C (case insensitive)
+    const regex = /^c/i;
+    this.filter ? this.users$ = this.bufferUsers$ : this.users$ = this.users$.pipe(map(user => user.filter(usr => usr.name.match(regex))));
+    this.filter = !this.filter;
   }
 }
